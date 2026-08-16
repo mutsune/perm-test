ab131249d1002850b6700e516fb0064d163f6c99
mutsune <mutsune@users.noreply.github.com>
2026-08-16 10:12:57 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = 'shell-v1';
+const SHELL = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(hit =>
+      hit || fetch(e.request).catch(() =>
+        e.request.mode === 'navigate' ? caches.match('./index.html') : Response.error()
+      )
+    )
+  );
+});
